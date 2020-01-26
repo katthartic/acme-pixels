2579ee4ef05fe2d872b2feef86fd467f81a3b35b
katthartic <katt.baum@gmail.com>
2020-01-26 11:30:35 -0500

    Add colorPalatte

--- acme-pixels.js
+++ acme-pixels.js
@@ -6,3 +6,25 @@ const hex = [
     'CC',
     'FF'
 ]
+
+const colorIterater = () => {
+    const colorList = []
+        for (let i = 0; i < hex.length; i++) {
+            let iColor = hex[i]
+
+            for (let j = 0; j < hex.length; j++) {
+                let jColor = iColor + hex[j]
+
+                for (let k = 0; k < 6; k++) {
+                    let kColor = jColor + hex[k]
+                    colorList.push(kColor)
+
+                }
+            }
+        }
+    return colorList
+}
+
+const colorPalatte = colorIterater()
+
+console.log(colorPalatte)
